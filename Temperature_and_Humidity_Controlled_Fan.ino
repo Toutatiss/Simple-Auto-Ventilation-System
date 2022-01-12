7878c75ca0360ed8317b6798dfe702bd1a5b8a95
Toutatiss <48107965+Toutatiss@users.noreply.github.com>
2022-01-12 18:39:34 +1100

    Add files via upload

---
+++ Temperature_and_Humidity_Controlled_Fan.ino
@@ -0,0 +1,80 @@
+#include <DHT.h>
+
+#define dhtDataPin 2
+#define fanEnablePin 6
+#define powerOnLEDPin 5
+#define highTempLEDPin 3
+#define highHumLEDPin 4
+
+#define DHT_Type DHT22
+
+DHT dht(dhtDataPin, DHT_Type);
+
+float hum;
+float temp;
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(fanEnablePin, OUTPUT);
+  
+  pinMode(powerOnLEDPin, OUTPUT);
+  pinMode(highTempLEDPin, OUTPUT);
+  pinMode(highHumLEDPin, OUTPUT);
+
+  // INDICATE POWER ON
+  digitalWrite(powerOnLEDPin, HIGH);
+
+  // FAN TEST
+  digitalWrite(fanEnablePin, HIGH);
+  delay(2000);
+  digitalWrite(fanEnablePin, LOW);
+
+  // START UP DHTSENSOR
+  dht.begin();
+}
+
+void loop() {
+  getTempHum();
+
+  // FAN ENABLE:
+  if ((temp > 25.00)||(hum > 98.00)) {
+    digitalWrite(fanEnablePin, HIGH);
+  } else {
+    digitalWrite(fanEnablePin, LOW);
+  }
+
+  // INDICATOR LIGHTS:
+  getLightSettings();
+  
+  delay(1000);
+}
+
+
+
+
+
+void  getTempHum() {
+  hum = dht.readHumidity();
+  temp = dht.readTemperature();
+  Serial.print(hum);
+  Serial.print(" % RH\n");
+  Serial.print(temp);
+  Serial.print(" degrees C\n\n");
+}
+
+void getLightSettings() {
+  if ((temp > 25.00)&&(hum > 60.00)) {
+    digitalWrite(highTempLEDPin, HIGH);
+    digitalWrite(highHumLEDPin, HIGH);
+  } else if (hum > 60.00) {
+    digitalWrite(highTempLEDPin, LOW);
+    digitalWrite(highHumLEDPin, HIGH);
+  } else if (temp > 25.00) {
+    digitalWrite(highTempLEDPin, HIGH);
+    digitalWrite(highHumLEDPin, LOW);
+  } else {
+    digitalWrite(highTempLEDPin, LOW);
+    digitalWrite(highHumLEDPin, LOW);
+  }
+}
